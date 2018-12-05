4e0ba66947cb1d1c75e153e0a2f334544b9cf6fd
Tom Hall <tom.hall@permutive.com>
2018-12-05 17:24:30 +0000

    First cut

---
+++ BindingTest/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Example.Testlib;
+//using IO.Reactivex;
+//using Com.Squareup.Duktape;
+
+
+namespace BindingTest
+{
+    [Activity(Label = "BindingTest", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            //TestJava test = new TestJava();
+            TestKotlin testKotlin = new TestKotlin();
+
+            ////button.Text = "wha: "+ testKotlin.ReturnSomething();
+            //button.Text = "wha: " + testKotlin.TestWithArrow();
+            button.Text = "huhwa: " + testKotlin.TestWithDuktape();
+
+            //button.Text = "rx: " + Single.Just("Rx Hullo").BlockingGet();
+
+            button.Text = "rx: " + testKotlin.TestWithRx();
+
+            //Duktape dt = Duktape.Create();
+
+            //button.Text = (string) dt.Evaluate("'hello world'.toUpperCase();");
+
+            //button.Text = "hey";
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
