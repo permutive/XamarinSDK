7dc537f47b51b4cd5223c7f7d964da01505dd53d
Tom Hall <tom.hall@permutive.com>
2018-12-11 11:27:05 +0000

    Clean up test code

--- BindingTest/MainActivity.cs
+++ BindingTest/MainActivity.cs
@@ -13,13 +13,6 @@ using Java.Lang;
 using Android.Runtime;
 using Com.Permutive.Android.Internal;
 
-//using Com.Example.Testlib;
-//using Android.Arch.Persistence.Room;
-
-//using IO.Reactivex;
-//using Com.Squareup.Duktape;
-
-
 namespace BindingTest
 {
     [Activity(Label = "BindingTest", MainLauncher = true, Icon = "@mipmap/icon")]
@@ -37,12 +30,6 @@ namespace BindingTest
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
-
-
-            UUID TRANTOR_STAGING_API_KEY = UUID.FromString("5c2b415d-7b20-4bc9-84fb-4f04bf0e5743");
-            UUID TRANTOR_STAGING_PROJECT_ID = UUID.FromString("be668577-07f5-444d-98e0-222b990951b1");
-
-
 
             bool useRelease = false;
 
@@ -118,37 +105,13 @@ namespace BindingTest
             TriggersProvider triggersProvider = permutive.TriggersProvider();
             TriggersProvider.TriggerAction querySegments = triggersProvider.QuerySegments(new SegmentListener());
 
-            //TriggersProvider.TriggerAction queryReactions = triggersProvider.QueryReactions("dfp", new ReactionsListener());
+            TriggersProvider.TriggerAction queryReactions = triggersProvider.QueryReactions("dfp", new ReactionsListener());
 
             TriggersProvider.TriggerAction specific = triggersProvider.InvokeTriggerAction(1068, new QueryListener());
 
             TriggersProvider.TriggerAction map = triggersProvider.TriggerActionMap(1068, new QueryListener());
 
             //queryReactions.Close();
-
-
-            /*
-            //TestJava test = new TestJava();
-            TestKotlin testKotlin = new TestKotlin();
-
-            button.Text = "wha: "+ testKotlin.ReturnSomething();
-            button.Text = "wha: " + testKotlin.TestWithArrow();
-            button.Text = "huhwa: " + testKotlin.TestWithDuktape();
-
-            //button.Text = "rx: " + Single.Just("Rx Hullo").BlockingGet();
-
-            button.Text = "rx: " + testKotlin.TestWithRx(); button.Text = "moshi2: " + testKotlin.TestMoshi();
-
-            button.Text = "retrofit3: " + testKotlin.TestRetrofit();
-
-            button.Text = "room: " + testKotlin.TestRoom(this);
-
-            //Duktape dt = Duktape.Create();
-
-            //button.Text = (string) dt.Evaluate("'hello world'.toUpperCase();");
-
-            //button.Text = "hey";
-            */
 
             button.Click += delegate { button.Text = $"{count++} clicks!"; };
         }
@@ -161,13 +124,6 @@ namespace BindingTest
         {
             Java.Util.IList segments = p0.JavaCast<Java.Util.IList>();
             Android.Util.Log.Debug("WTF", $"Segments updated: {segments}");
-
-            //foreach (var segment in segments) //doesn't work!
-            //for (int index = 0;  index < segments.Size();  index++)
-            //{
-            //    Android.Util.Log.Debug("WTF", $"getting {index}");
-            //    Android.Util.Log.Debug("WTF", $"{segments.Get(index)}");
-            //}
         }
     }
 
@@ -186,7 +142,6 @@ namespace BindingTest
 
         void IMethod.Invoke(Java.Lang.Object p0)
         {
-            //Java.Util.ArrayList segments = p0.JavaCast<Java.Util.ArrayList>();
             Android.Util.Log.Debug("WTF", $"Query updated: {p0} {p0.GetType()}");
         }
     }
