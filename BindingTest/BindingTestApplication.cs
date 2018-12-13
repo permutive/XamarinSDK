3a56604636eb009331b7fa9b33aab5bb19f2bab7
Tom Hall <tom.hall@permutive.com>
2018-12-13 15:11:02 +0000

    Move to release version of core aar

--- BindingTest/BindingTestApplication.cs
+++ BindingTest/BindingTestApplication.cs
@@ -9,7 +9,6 @@ namespace BindingTest
     [Application]
     public class BindingTestApplication : Application
     {
-        private bool useRelease = false;
         private Permutive.Xamarin.Permutive permutive;
 
         public BindingTestApplication(IntPtr handle, JniHandleOwnership ownerShip) : base(handle, ownerShip)
@@ -36,20 +35,15 @@ namespace BindingTest
         {
 
             PermutiveOptions options = new PermutiveOptions();
-
-            if (useRelease)
-            {
-                options.ApiKey= "e0039147-51e7-4224-a814-0e2d438aabcd";
-                options.ProjectId = "da4d09b5-843a-4bd5-bd79-8cea7f69f730";
-            }
-            else
-            {
-                options.ApiKey = "5c2b415d-7b20-4bc9-84fb-4f04bf0e5743";
-                options.ProjectId = "be668577-07f5-444d-98e0-222b990951b1";
-            }
+            options.ApiKey= "da4d09b5-843a-4bd5-bd79-8cea7f69f730";
+            options.ProjectId = "e0039147-51e7-4224-a814-0e2d438aabcd";
 
             var newPermutive = new PermutiveImpl(this);
             newPermutive.Initialize(options);
+
+
+            //when we want to shut down/stop processing, say when the app is destroyed, call:
+            //newPermutive.Dispose();
 
             return newPermutive;
         }
