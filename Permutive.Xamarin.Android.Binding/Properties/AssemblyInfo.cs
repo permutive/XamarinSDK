317007bdf865d93c7e8853143378d80d78a28ea2
Tom Hall <tom.hall@permutive.com>
2019-01-14 14:21:50 +0000

    Rename Android binding library

--- Permutive.Xamarin.Android.Binding/Properties/AssemblyInfo.cs
+++ Permutive.Xamarin.Android.Binding/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Permutive")]
+[assembly: AssemblyTitle("Permutive.Xamarin.Android.Binding")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
